7d22da07f867f2c29afcb16e1acd2cf05303362e
pnouvellet <p.nouvellet@imperial.ac.uk>
2023-06-16 16:04:30 +0100

    checks

--- R/f_z_NB.R
+++ R/f_z_NB.R
@@ -9,31 +9,31 @@
 #'
 #' @param R is the reproduction number, i.e. the average number of secondary cases due to a single case.
 #' This can be any positive number.
-#' 
+#'
 #' @param over is the overdispersion in the offspring distribution.
-#' 
+#'
 #' @return
 #'  The function returns a list including:
 #' \itemize{
 #'
 #' \item the probability of observing an outbreak of size z given R/overdispersion
-#' 
+#'
 #'}
 #'
-#' 
 #'
+#' see https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3821076/
 #'
 #' @examples
 #'
 #' x <- f_z_NB(z = 1:1e2, s = 1, R = .5, over = 1)
-#' 
-#' 
+#'
+#'
 #
 f_z_NB <- function(z,s,R,over){
 
   logf<-lgamma(z*over+z-1)-lgamma(z*over)-lgamma(z+1) + (z-1)*log(R/over) - (z*over+z-1)*log(R/over+1)
   f<-exp(logf)
-  
+
   # logf<-(z-s-1)*log( s*z)+(z-s)*log(R)+ (-z*R) - lgamma(z-s +1)
   # f<-exp(logf)
 
